ce38cab7a1f0341d92e42cf843801da12c6567a6
Tia King <tiajaneeking@gmail.com>
2018-09-05 10:07:43 -0700

    extracted JSON of products/information, doesn't match website for some reason

--- sephora-api.py
+++ sephora-api.py
@@ -26,14 +26,25 @@ soup.prettify()
 soup.getText()
 
 spans = (soup.find('div', {"class":"Main-content"}))
-text = spans.findAll('span', {"class": "u-hide"})[1]
-print(text)
-# new_text = text.replace("&quot;", " ")
+text = spans.findAll('span', {"class": "u-hide"})[1]['data-json']
+json_text = json.loads(text)
+# print(json_text)
+for keys in json_text:
+	print(json_text['sku_list'])
+# new_text = text.replace("&quot", " ")
+# huh_text = new_text.replace("'", "")
+# newer_text = new_text.replace(";", "")
+# product_list = (newer_text.split("},{"))
+
+# pprint.pprint(product_list)
+
+# print(text)
+
 # sub_soup = BeautifulSoup(str(spans))
 # post_soup = sub_soup.findAll("div",{"class":"SkuItem-info"})
-if text is not None:
-	print("yerr")
-else:
-	print("nurrr")
+# if text is not None:
+# 	print("yerr")
+# else:
+# 	print("nurrr")
 # if __name__ == "__main__":
 #     main()
